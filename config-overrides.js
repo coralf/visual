5da04233eaf7e042c266a84c580b4f6a1e90d790
fanzhengmao <fanzhengmao@huohua.cn>
2021-08-04 22:27:24 +0800

    chore: 初始项目

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const rewireLess = require("react-app-rewire-less");
+
+module.exports = function override(config, env) {
+  config = rewireLess.withLoaderOptions({
+    javascriptEnabled: true,
+  })(config, env);
+
+  return config;
+};
